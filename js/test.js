912b1da858fdde8c81e5d93e243b6b22942b06f2
Maryan121 <88990104+Maryan121@users.noreply.github.com>
2023-07-05 17:53:10 +0300

    Delete test.js

--- js/test.js
+++
@@ -1,7 +0,0 @@
-window.onresize = screen;
-window.onload = screen;
-
-function screen(){
-    myWindth = window.innerWidth;
-    document.getElementById('size').innerHTML = `width: ${myWindth} px`;
-}
